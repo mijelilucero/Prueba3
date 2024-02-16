6b15f82e0eaec7020edcc8f1e1bef77c9e2387ea
Mijeli <mlucerob@miumg.edu.gt>
2024-02-16 16:27:09 -0600

    Hola mundo

--- PrograTarea#2/Program.cs
+++ PrograTarea#2/Program.cs
@@ -1,6 +1,2 @@
-﻿Console.WriteLine("Ingrese un valor entero: ");
-
-int numInt = Convert.ToInt32(Console.ReadLine());
-decimal numDec = (decimal)numInt;
-
-Console.WriteLine("El valor entero ingresado convertido a decimal es: " + numDec);
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
